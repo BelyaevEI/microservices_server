564724804f2ff27e45b39557f968130798f18083
egorb <egor.belyaev95@yandex.ru>
2024-07-22 21:24:26 +0300

    2 homework is completed

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
+ARG CONFIG_PATH
+
 FROM golang:1.20.3-alpine AS builder
 
 WORKDIR /app
@@ -10,5 +12,7 @@ FROM alpine:latest
 
 WORKDIR /app
 COPY --from=builder /app/bin/crud_server .
+COPY local.env .
+COPY prod.env .
 
 CMD ["./crud_server"]
--- cmd/main.go
+++ cmd/main.go
@@ -2,56 +2,156 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"flag"
 	"log"
 	"net"
+	"os"
 
-	"github.com/brianvoe/gofakeit"
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v4/pgxpool"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
 
+	"github.com/BelyaevEI/microservices_server/internal/config"
 	desc "github.com/BelyaevEI/microservices_server/pkg/chat_v1"
+	sq "github.com/Masterminds/squirrel"
 )
 
-const grpcPort = 50052
+var configPath string
 
 type server struct {
 	desc.UnimplementedChatV1Server
+	pool *pgxpool.Pool
+}
+
+func init() {
+	configPath = os.Getenv("CONFIG_PATH")
 }
 
 // Create chat
-func (s *server) CreateChat(_ context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
-	log.Printf("Create usernames: %s", req.GetUsernames())
+func (s *server) CreateChat(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+
+	if req.GetChatname() == "" {
+		return nil, status.Error(codes.InvalidArgument, "chat name is empty")
+	}
+
+	builderInsert := sq.Insert("chats").
+		PlaceholderFormat(sq.Dollar).
+		Columns("name", "user_ids").
+		Values(req.GetChatname(), req.GetId()).
+		Suffix("RETURNING id")
+
+	query, args, err := builderInsert.ToSql()
+	if err != nil {
+		log.Printf("create chat build is failed: %e", err)
+		return nil, status.Error(codes.Internal, "create chat build is failed")
+	}
+
+	var chatID int64
+	err = s.pool.QueryRow(ctx, query, args...).Scan(&chatID)
+	if err != nil {
+		log.Printf("err: %e", err)
+		return nil, status.Error(codes.Internal, "failed to insert table chats")
+	}
 
 	return &desc.CreateResponse{
-		Id: int64(gofakeit.Number(1, 1000)),
+		Id: chatID,
 	}, nil
 }
 
 // Delete chat
-func (s *server) DeleteChat(_ context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
-	log.Printf("Delete chat with id: %v", req.GetId())
+func (s *server) DeleteChat(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
 
+	builderDelete := sq.Delete("chats").
+		PlaceholderFormat(sq.Dollar).
+		Where(sq.Eq{"id": req.GetId()})
+
+	query, args, err := builderDelete.ToSql()
+	if err != nil {
+		log.Printf("delete chat build is failed: %v", err)
+		return nil, status.Error(codes.Internal, "failed to build query")
+	}
+
+	res, err := s.pool.Exec(ctx, query, args...)
+	if err != nil {
+		log.Printf("failed to delete chat: %v", err)
+		return nil, status.Error(codes.Internal, "failed to delete chat")
+	}
+
+	log.Printf("deleted %d rows", res.RowsAffected())
 	return nil, nil
 }
 
 // Send message to server
-func (s *server) SendMessage(_ context.Context, req *desc.SendMessageRequest) (*emptypb.Empty, error) {
-	log.Printf("Send message %s from %s at %v", req.Text, req.Text, req.Timestamp)
+func (s *server) SendMessage(ctx context.Context, req *desc.SendMessageRequest) (*desc.SendMessageResponse, error) {
 
-	return nil, nil
+	if req.GetText() == "" {
+		return nil, status.Error(codes.InvalidArgument, "message is empty")
+	}
+
+	builderInsert := sq.Insert("message").
+		PlaceholderFormat(sq.Dollar).
+		Columns("chat_id", "user_id", "text").
+		Values(req.GetToChatId(), req.GetFromUserId(), req.GetText()).
+		Suffix("RETURNING id")
+
+	query, args, err := builderInsert.ToSql()
+	if err != nil {
+		log.Printf("err: %e", err)
+		return nil, status.Error(codes.Internal, "send message failed to build query")
+	}
+	var messageID uuid.UUID
+	err = s.pool.QueryRow(ctx, query, args...).Scan(&messageID)
+	if err != nil {
+		log.Printf("err: %e", err)
+		return nil, status.Error(codes.Internal, "send message failed to insert message")
+	}
+
+	log.Printf("inserted chat with id: %d", messageID)
+
+	return &desc.SendMessageResponse{
+		Id:     messageID.String(),
+		ChatId: req.GetToChatId(),
+	}, nil
 }
 
 func main() {
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+
+	ctx := context.Background()
+	flag.Parse()
+
+	err := config.Load(configPath)
+	if err != nil {
+		log.Fatalf("load config is failed: %v", err)
+	}
+
+	grpcConfig, err := config.NewGRPCConfig()
+	if err != nil {
+		log.Fatalf("failed to get grpc config: %v", err)
+	}
+
+	pgConfig, err := config.NewPGConfig()
+	if err != nil {
+		log.Fatalf("failed to get pg config: %v", err)
+	}
+
+	lis, err := net.Listen("tcp", grpcConfig.Address())
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
+	pool, err := pgxpool.Connect(ctx, pgConfig.DSN())
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+	defer pool.Close()
+
 	s := grpc.NewServer()
 	reflection.Register(s)
-	desc.RegisterChatV1Server(s, &server{})
+	desc.RegisterChatV1Server(s, &server{pool: pool})
 
 	log.Printf("server listening at %v", lis.Addr())
 
